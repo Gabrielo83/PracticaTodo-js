c181b9470f0b00f689bcfff29d3a0a2d80828a3c
Gabriel Caamaño <gcaamano83@gmail.com>
2023-06-09 00:29:38 -0300

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-import { saludar } from "./js/componentes.js";
 import "./styles.css";
+import { Todo, TodoList } from "./classes/index";
+import { crearTodoHtml } from "./js/componentes";
 
-const nombre = "Gabriel";
-const apellido = "Caamaño";
+export const todoList = new TodoList();
+// const tarea = new Todo("Aprender Javascript");
 
-saludar(nombre, apellido);
+// todoList.nuevoTodo(tarea);
+
+// console.log(todoList);
+
+// crearTodoHtml(tarea);
+
+// localStorage.setItem("my-key", "Prueba123");
+// sessionStorage.setItem("my-key2", "Prueba123");
+
+// setTimeout(() => {
+//   localStorage.removeItem("my-key");
+// }, 3000);
+
+todoList.todos.forEach((todo) => {
+  crearTodoHtml(todo);
+});
